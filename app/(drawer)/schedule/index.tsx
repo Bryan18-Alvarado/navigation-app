531175c3e9f340cd61c5c66a25480dfa30447904
Bryan18-Alvarado <bryanalvarado1904@gmail.com>
2026-05-21 12:21:41 -0600

    feat: mejorar diseño de la pantalla de horario con alineación y estilo del texto

--- app/(drawer)/schedule/index.tsx
+++ app/(drawer)/schedule/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from 'react-native'
 
 const ScheduleScreen = () => {
   return (
-    <View>
-      <Text>Schedule Screen</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="font-work-black text-3xl">Schedule Screen</Text>
     </View>
   )
 }
